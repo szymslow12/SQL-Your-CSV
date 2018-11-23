b314009803a577cb2a3bcd3f1daa426d84f11467
szymslow12 <szym.slow12@gmail.com>
2018-11-23 22:30:53 +0100

    add showMenu()

--- src/main/java/com/codecool/SQLYourCSV/view/MenuView.java
+++ src/main/java/com/codecool/SQLYourCSV/view/MenuView.java
@@ -5,4 +5,9 @@ public class MenuView implements Alertable {
     public void alert(String msg) {
         System.out.println(String.format("\u001B[31m%s\u001B[0m", msg));
     }
+
+
+    public void showMenu() {
+
+    }
 }

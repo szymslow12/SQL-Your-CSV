5e2c1efc00bb36419638918cb8dcc6ff242838f0
szymslow12 <szym.slow12@gmail.com>
2018-11-23 14:25:38 +0100

    add fields -> MenuView, UserInputs

--- src/main/java/com/codecool/SQLYourCSV/controller/AppController.java
+++ src/main/java/com/codecool/SQLYourCSV/controller/AppController.java
@@ -1,6 +1,18 @@
 package com.codecool.SQLYourCSV.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AppController {
+
+    @Autowired
+    private MenuView view;
+
+    @Autowired
+    private UserInputs inputs;
+
+
+
+
 
     public void run() {
 

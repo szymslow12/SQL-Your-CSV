5f84357796a25d9f0666544add10d7aae056db0d
szymslow12 <szym.slow12@gmail.com>
2018-11-23 14:43:22 +0100

    add getNumber

--- src/main/java/com/codecool/SQLYourCSV/model/UserInputs.java
+++ src/main/java/com/codecool/SQLYourCSV/model/UserInputs.java
@@ -1,10 +1,27 @@
 package com.codecool.SQLYourCSV.model;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class UserInputs {
 
     public String getString() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+
+
+    public int getNumber() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isCorrectInput = false;
+        Integer toReturn = 0;
+        while (!isCorrectInput) {
+            try {
+                toReturn = scanner.nextInt();
+                isCorrectInput = true;
+            } catch (InputMismatchException err) {
+                System.out.println("Wrong input, number needed");
+            }
+        }
+        return toReturn;
     }
 }

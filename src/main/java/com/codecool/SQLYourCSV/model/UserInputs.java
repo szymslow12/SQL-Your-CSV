824b4b93db70227312200ba495978a3fed7cff9e
szymslow12 <szym.slow12@gmail.com>
2018-11-23 14:35:05 +0100

    add getString

--- src/main/java/com/codecool/SQLYourCSV/model/UserInputs.java
+++ src/main/java/com/codecool/SQLYourCSV/model/UserInputs.java
@@ -1,4 +1,10 @@
 package com.codecool.SQLYourCSV.model;
 
+import java.util.Scanner;
 public class UserInputs {
+
+    public String getString() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
 }

4d828db4f0e640f1707af414f5a739116be55707
szymslow12 <szym.slow12@gmail.com>
2018-11-23 23:05:18 +0100

    add constant menu label "Exit program", change numbers of labels (i+1)

--- src/main/java/com/codecool/SQLYourCSV/view/MenuView.java
+++ src/main/java/com/codecool/SQLYourCSV/view/MenuView.java
@@ -14,7 +14,8 @@ public class MenuView implements Alertable {
 
     public void showMenu(String[] labels) {
         alert("MAIN MENU\n");
-        IntStream.range(0, labels.length).forEach(i -> alert(String.format("\t%s. %s", i, labels[i])));
+        IntStream.range(0, labels.length).forEach(i -> alert(String.format("\t%s. %s", i + 1, labels[i])));
+        alert("\t0. Exit program");
         System.out.print("\n\u001B[31mEnter a number: \u001B[0m");
     }
 }

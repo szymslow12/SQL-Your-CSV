83b70a70313c33aa8a23eef08df9ce2bae4956b1
szymslow12 <szym.slow12@gmail.com>
2018-11-24 14:19:53 +0100

    add fields and setters

--- src/main/java/com/codecool/SQLYourCSV/controller/QueryController.java
+++ src/main/java/com/codecool/SQLYourCSV/controller/QueryController.java
@@ -1,4 +1,31 @@
 package com.codecool.SQLYourCSV.controller;
 
+import com.codecool.SQLYourCSV.model.UserInputs;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class QueryController {
+
+    @Autowired
+    private DataView view;
+
+    @Autowired
+    private UserInputs inputs;
+
+    @Autowired
+    private TableService service;
+
+
+    public void setView(DataView view) {
+        this.view = view;
+    }
+
+
+    public void setUserInputs(UserInputs inputs) {
+        this.inputs = inputs;
+    }
+
+
+    public void setService(TableService service) {
+        this.service = service;
+    }
 }

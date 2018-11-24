6dec743f3776418ecb4e8fa219e16e203923214e
szymslow12 <szym.slow12@gmail.com>
2018-11-24 14:31:44 +0100

    implement alert()

--- src/main/java/com/codecool/SQLYourCSV/view/DataView.java
+++ src/main/java/com/codecool/SQLYourCSV/view/DataView.java
@@ -1,4 +1,8 @@
 package com.codecool.SQLYourCSV.view;
 
-public class DataView {
+public class DataView implements Alertable {
+
+    public void alert(String msg) {
+        System.out.println(String.format("\u001B[36m%s\u001B[0m", msg));
+    }
 }

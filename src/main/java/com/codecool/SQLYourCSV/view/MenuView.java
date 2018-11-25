20097adb5f75931e82921dcb55442550b4ac908d
szymslow12 <szym.slow12@gmail.com>
2018-11-25 02:38:42 +0100

    change welcome text in menu

--- src/main/java/com/codecool/SQLYourCSV/view/MenuView.java
+++ src/main/java/com/codecool/SQLYourCSV/view/MenuView.java
@@ -13,7 +13,7 @@ public class MenuView implements Alertable {
 
 
     public void showMenu(String[] labels) {
-        alert("MAIN MENU\n");
+        alert("WELCOME IN MAIN MENU\n");
         IntStream.range(0, labels.length).forEach(i -> alert(String.format("\t%s. %s", i + 1, labels[i])));
         alert("\t0. Exit program");
         System.out.print("\n\u001B[31mEnter a number: \u001B[0m");

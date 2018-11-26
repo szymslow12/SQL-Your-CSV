171d72a39f5479ddf929c53b3cd03014f4a3ada5
szymslow12 <szym.slow12@gmail.com>
2018-11-26 13:23:30 +0100

    add readFile, readFiles and needed imports

--- src/main/java/com/codecool/SQLYourCSV/model/data/FileReader.java
+++ src/main/java/com/codecool/SQLYourCSV/model/data/FileReader.java
@@ -1,4 +1,16 @@
 package com.codecool.SQLYourCSV.model.data;
 
+import java.util.List;
+import java.util.Map;
+
 public class FileReader {
+
+    public static List<String[]> readFile(String file) {
+        return null;
+    }
+
+
+    public static Map<String, List<String[]>> readFiles(String[] files) {
+        return null;
+    }
 }

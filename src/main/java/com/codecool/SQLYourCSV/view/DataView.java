e0298ae02316fb6c00c49cf8f1c36c0e84b808ee
szymslow12 <szym.slow12@gmail.com>
2018-11-26 12:10:35 +0100

    add showTable

--- src/main/java/com/codecool/SQLYourCSV/view/DataView.java
+++ src/main/java/com/codecool/SQLYourCSV/view/DataView.java
@@ -8,4 +8,8 @@ public class DataView implements Alertable {
     public void alert(String msg) {
         System.out.println(String.format("\u001B[36m%s\u001B[0m", msg));
     }
+
+    public void showTable(Table table) {
+        alert("Table shown");
+    }
 }

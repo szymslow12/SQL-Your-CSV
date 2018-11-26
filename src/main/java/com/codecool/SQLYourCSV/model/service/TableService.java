8ce1e76f1da45fba184a02c6fe444392d52c6fc1
szymslow12 <szym.slow12@gmail.com>
2018-11-26 12:34:28 +0100

    add import model.CSVData

--- src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
+++ src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
@@ -1,5 +1,6 @@
 package com.codecool.SQLYourCSV.model.service;
 
+import com.codecool.SQLYourCSV.model.CSVData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

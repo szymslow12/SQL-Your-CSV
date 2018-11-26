eee7f60289b3906e6d67a0db95bc679c84bd6a62
szymslow12 <szym.slow12@gmail.com>
2018-11-26 12:18:11 +0100

    add CSVData with setter/getter

--- src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
+++ src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
@@ -1,7 +1,20 @@
 package com.codecool.SQLYourCSV.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TableService {
+
+    @Autowired
+    private CSVData data;
+
+    public void setData(CSVData data) {
+        this.data = data;
+    }
+
+
+    public CSVData getData() {
+        return data;
+    }
 }

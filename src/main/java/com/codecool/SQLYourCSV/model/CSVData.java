5d7ba100a36d79ae611abd0a4df2073fe42ec6f5
szymslow12 <szym.slow12@gmail.com>
2018-11-26 12:34:01 +0100

    add field Map<String, List<String[]>> data, getter, needed imports

--- src/main/java/com/codecool/SQLYourCSV/model/CSVData.java
+++ src/main/java/com/codecool/SQLYourCSV/model/CSVData.java
@@ -2,6 +2,14 @@ package com.codecool.SQLYourCSV.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class CSVData {
+    private Map<String, List<String[]>> data;
+
+    public Map<String, List<String[]>> getData() {
+        return data;
+    }
 }

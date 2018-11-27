70358a4fc0cc645ac23024194eb895744b1d78ba
szymslow12 <szym.slow12@gmail.com>
2018-11-27 13:42:45 +0100

    add shouldShowMenuWithoutGivenLabels, String fields TOP_MENU_LINE, EXIT_PROGRAM_LINE, END_LINE

--- src/test/java/com/codecool/SQLYourCSV/view/MenuViewTest.java
+++ src/test/java/com/codecool/SQLYourCSV/view/MenuViewTest.java
@@ -17,6 +17,9 @@ class MenuViewTest {
     private MenuView view;
     private final String RED = "\u001B[31m";
     private final String DEFAULT_COLOR = "\u001B[0m";
+    private final String TOP_MENU_LINE = String.format("%s%s%s%n", RED, "WELCOME IN MAIN MENU\n", DEFAULT_COLOR);
+    private final String EXIT_PROGRAM_LINE = String.format("%s%s%s%n", RED, "\t0. Exit program", DEFAULT_COLOR);
+    private final String END_LINE = String.format("%n%s%s%s", RED, "Enter a number: ", DEFAULT_COLOR);
 
     @BeforeEach
     void initPrivateFields() {
@@ -25,6 +28,7 @@ class MenuViewTest {
         System.setOut(new PrintStream(os));
         view = new MenuView();
     }
+
 
     @AfterEach
     void setPreviousPrintStream() {
@@ -42,4 +46,22 @@ class MenuViewTest {
 
         assertEquals(expected, actual);
     }
+
+
+    @Test
+    void shouldShowMenuWithoutGivenLabels() {
+        String[] labels = new String[0];
+        view.showMenu(labels);
+
+        String expected = String.format("%s%s%s",
+                TOP_MENU_LINE,
+                EXIT_PROGRAM_LINE,
+                END_LINE);
+
+        String actual = os.toString();
+
+        assertEquals(expected, actual);
+    }
+
+
 }

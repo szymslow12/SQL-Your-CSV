2f48c443ff8e051b9f215d92a5e3e4c68d8f453f
szymslow12 <szym.slow12@gmail.com>
2018-11-27 16:56:20 +0100

    add initPrivateFields, setPreviousPrintStream

--- src/test/java/com/codecool/SQLYourCSV/view/DataViewTest.java
+++ src/test/java/com/codecool/SQLYourCSV/view/DataViewTest.java
@@ -1,7 +1,32 @@
 package com.codecool.SQLYourCSV.view;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataViewTest {
+
+    private ByteArrayOutputStream os;
+    private PrintStream prevPs;
+
+    private DataView view;
+
+    @BeforeEach
+    void initPrivateFields() {
+        prevPs = System.out;
+        os = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(os));
+        view = new DataView();
+    }
+
+
+    @AfterEach
+    void setPreviousPrintStream() {
+        System.setOut(prevPs);
+    }
 
 }

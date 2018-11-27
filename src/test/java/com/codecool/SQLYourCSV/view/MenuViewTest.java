fe17606481b158b9caa2c853c0837707bd74376b
szymslow12 <szym.slow12@gmail.com>
2018-11-27 14:06:15 +0100

    add getExpectedMenu, shouldShowMenuWithPassedOneLabel

--- src/test/java/com/codecool/SQLYourCSV/view/MenuViewTest.java
+++ src/test/java/com/codecool/SQLYourCSV/view/MenuViewTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +22,7 @@ class MenuViewTest {
     private final String TOP_MENU_LINE = String.format("%s%s%s%n", RED, "WELCOME IN MAIN MENU\n", DEFAULT_COLOR);
     private final String EXIT_PROGRAM_LINE = String.format("%s%s%s%n", RED, "\t0. Exit program", DEFAULT_COLOR);
     private final String END_LINE = String.format("%n%s%s%s", RED, "Enter a number: ", DEFAULT_COLOR);
+    private final String TAB = "\t";
 
     @BeforeEach
     void initPrivateFields() {
@@ -53,11 +56,33 @@ class MenuViewTest {
         String[] labels = new String[0];
         view.showMenu(labels);
 
-        String expected = String.format("%s%s%s", TOP_MENU_LINE, EXIT_PROGRAM_LINE, END_LINE);
+        String expected = getExpectedMenu(labels);
         String actual = os.toString();
 
         assertEquals(expected, actual);
     }
 
 
+    @Test
+    void shouldShowMenuWithPassedOneLabel() {
+        String[] label = {"Menu label 1"};
+        view.showMenu(label);
+
+        String expected = getExpectedMenu(label);
+        String actual = os.toString();
+
+        assertEquals(expected, actual);
+    }
+
+
+    private String getExpectedMenu(String[] labels) {
+        StringBuilder sB = new StringBuilder(TOP_MENU_LINE);
+        //zrobic pierdolony clean tych linii
+        IntStream.range(0, labels.length).forEach
+            (i -> sB.append(String.format("%s%s%s%n", RED,
+                    String.format("%s%s. %s", TAB, i + 1, labels[i]), DEFAULT_COLOR)));
+        sB.append(EXIT_PROGRAM_LINE);
+        sB.append(END_LINE);
+        return sB.toString();
+    }
 }

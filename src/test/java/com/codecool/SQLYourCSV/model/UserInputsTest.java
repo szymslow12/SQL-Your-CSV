43ca3c1c72477432266ace852fcd1a3c44533fde
szymslow12 <szym.slow12@gmail.com>
2018-11-27 16:30:34 +0100

    add UserInputsTest initPrivateFields,setPrevIS,shouldGetStringReturnString

---
+++ src/test/java/com/codecool/SQLYourCSV/model/UserInputsTest.java
@@ -0,0 +1,37 @@
+package com.codecool.SQLYourCSV.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInputsTest {
+
+    private InputStream prevIS;
+    private UserInputs userInputs;
+
+    @BeforeEach
+    void initPrivateFields() {
+        prevIS = System.in;
+        userInputs = new UserInputs();
+    }
+
+
+    @AfterEach
+    void setPrevIS() {
+        System.setIn(prevIS);
+    }
+
+    @Test
+    void shouldGetStringReturnString() {
+        String expected = "expected result";
+        System.setIn(new ByteArrayInputStream(expected.getBytes()));
+        String actual = userInputs.getString();
+
+        assertEquals(expected, actual);
+    }
+}

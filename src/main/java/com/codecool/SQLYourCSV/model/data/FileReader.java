ceb32eba4853f5cf5a4219fa793fc7fc0b7243ec
szymslow12 <szym.slow12@gmail.com>
2018-11-28 11:38:06 +0100

    add variable url and if stmt which check if url is null throws exception

--- src/main/java/com/codecool/SQLYourCSV/model/data/FileReader.java
+++ src/main/java/com/codecool/SQLYourCSV/model/data/FileReader.java
@@ -2,6 +2,7 @@ package com.codecool.SQLYourCSV.model.data;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.*;
 
 public class FileReader {
@@ -9,7 +10,9 @@ public class FileReader {
     public static List<String[]> readFile(String file) {
         List<String[]> readFile = new ArrayList<>();
         try {
-            Scanner scanner = new Scanner(new File(FileReader.class.getClassLoader().getResource(file).getFile()));
+            URL url = FileReader.class.getClassLoader().getResource(file);
+            if (url == null) throw new FileNotFoundException("File not found!");
+            Scanner scanner = new Scanner(new File(url.getFile()));
             while (scanner.hasNext()) {
                 readFile.add(scanner.nextLine().split(",|:|;|\t"));
             }

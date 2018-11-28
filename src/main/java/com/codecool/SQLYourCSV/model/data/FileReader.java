1596093d9968e03b78de99c9bccee0a0a2843a38
szymslow12 <szym.slow12@gmail.com>
2018-11-28 13:05:35 +0100

    change readFile -> throws IOException and has only two lines

--- src/main/java/com/codecool/SQLYourCSV/model/data/FileReader.java
+++ src/main/java/com/codecool/SQLYourCSV/model/data/FileReader.java
@@ -7,19 +7,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FileReader {
 
-    public static List<String[]> readFile(String file) {
-        List<String[]> readFile = new ArrayList<>();
-        try {
-
-            Files.lines(getFilePath(file)).forEach(line -> readFile.add(line.split(",|:|;|\t")));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return readFile;
+    public static List<String[]> readFile(String file) throws IOException {
+        if (file == null) throw new IllegalArgumentException("Expect String filename: got null");
+        return Files.lines(getFilePath(file)).map(line -> line.split(",|:|;|\t")).collect(Collectors.toList());
     }
 
 

0b5e2ef6ff461480f8c0a5c95401462651494789
szymslow12 <szym.slow12@gmail.com>
2018-11-28 10:23:09 +0100

    add shouldReadFile, getExpectedListResult

--- src/test/java/com/codecool/SQLYourCSV/model/data/FileReaderTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/data/FileReaderTest.java
@@ -1,7 +1,21 @@
 package com.codecool.SQLYourCSV.model.data;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileReaderTest {
 
+
+    @Test
+    void shouldReadFile() {
+        List<String[]> expected = getExpectedListResult();
+    }
+
+
+    private List<String[]> getExpectedListResult() {
+        return null;
+    }
 }

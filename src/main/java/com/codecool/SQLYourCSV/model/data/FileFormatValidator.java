6a4a65caa4df8ae06072ce8680b8e5e773c8f560
szymslow12 <szym.slow12@gmail.com>
2018-11-28 16:13:13 +0100

    add comment

--- src/main/java/com/codecool/SQLYourCSV/model/data/FileFormatValidator.java
+++ src/main/java/com/codecool/SQLYourCSV/model/data/FileFormatValidator.java
@@ -1,5 +1,6 @@
 package com.codecool.SQLYourCSV.model.data;
 
+//Interface Should Be Fine for this -> interface FileFormatValidator -> CSVFormatValidator :D
 public class FileFormatValidator {
 
     public static boolean validate(String toValid) {

95bb5289e2b4dae06ce75c97bb7e4a81795966f3
szymslow12 <szym.slow12@gmail.com>
2018-11-29 11:06:04 +0100

    add Table with fields headers, rows, service, name, getters/setters

--- src/main/java/com/codecool/SQLYourCSV/model/datapresentation/Table.java
+++ src/main/java/com/codecool/SQLYourCSV/model/datapresentation/Table.java
@@ -1,4 +1,52 @@
 package com.codecool.SQLYourCSV.model.datapresentation;
 
+import com.codecool.SQLYourCSV.model.service.RowService;
+
+import java.util.List;
+
 public class Table {
+
+    private Row headers;
+    private List<Row> rows;
+    private RowService service;
+    private String name;
+
+    public void setHeaders(Row headers) {
+        this.headers = headers;
+    }
+
+
+    public Row getHeaders() {
+        return headers;
+    }
+
+
+    public void setService(RowService service) {
+        this.service = service;
+    }
+
+
+    public RowService getService() {
+        return service;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setRows(List<Row> rows) {
+        this.rows = rows;
+    }
+
+
+    public List<Row> getRows() {
+        return rows;
+    }
 }

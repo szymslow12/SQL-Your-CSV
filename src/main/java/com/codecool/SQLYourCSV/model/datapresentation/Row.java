f1a9d14e61c6a270b43a53558e23a5a1b7e215d2
szymslow12 <szym.slow12@gmail.com>
2018-11-29 10:35:07 +0100

    add Row with fields columns,service,primaryKey,setters/getters,toString

---
+++ src/main/java/com/codecool/SQLYourCSV/model/datapresentation/Row.java
@@ -0,0 +1,59 @@
+package com.codecool.SQLYourCSV.model.datapresentation;
+
+import com.codecool.SQLYourCSV.model.service.ColumnService;
+
+import java.util.List;
+
+public class Row {
+
+    private List<Column<?>> columns;
+    private Column<?> primaryKey;
+    private ColumnService service;
+
+    public void setPrimaryKey(Column<?> primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+
+    public Column<?> getPrimaryKey() {
+        return primaryKey;
+    }
+
+
+    public void setService(ColumnService service) {
+        this.service = service;
+    }
+
+
+    public ColumnService getService() {
+        return service;
+    }
+
+
+    public List<Column<?>> getColumns() {
+        return columns;
+    }
+
+
+    public void setColumns(List<Column<?>> columns) {
+        this.columns = columns;
+    }
+
+
+    public int size() {
+        return columns.size();
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sB = new StringBuilder("|");
+
+        columns.stream().forEach(
+            column -> sB.append(
+                String.format(" name = %s, value = %s |",column.getName(), column.getValue())
+            )
+        );
+        return sB.toString();
+    }
+}

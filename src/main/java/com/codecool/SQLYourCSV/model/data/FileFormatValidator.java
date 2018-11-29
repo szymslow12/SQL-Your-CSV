2df79ed2936ce543028a07e62cd3db3239e9b15c
szymslow12 <szym.slow12@gmail.com>
2018-11-29 07:19:07 +0100

    add static field String[] possibleFormats

--- src/main/java/com/codecool/SQLYourCSV/model/data/FileFormatValidator.java
+++ src/main/java/com/codecool/SQLYourCSV/model/data/FileFormatValidator.java
@@ -1,10 +1,18 @@
 package com.codecool.SQLYourCSV.model.data;
 
+import java.util.stream.Stream;
+
 //Interface Should Be Fine for this -> interface FileFormatValidator -> CSVFormatValidator :D
 public class FileFormatValidator {
+    //ADD THIS FIELD TO UML!
+    private static String[] possibleFormats = {
+        "^([\\w\\d-_@! ]+,{1})+([\\w\\d-_@!]+)$",
+        "^([\\w\\d-_@! ]+;{1})+([\\w\\d-_@!]+)$",
+        "^([\\w\\d-_@! ]+\t{1})+([\\w\\d-_@!]+)$"
+    };
 
     public static boolean validate(String toValid) {
         if (toValid == null) throw new IllegalArgumentException("Expect String toValid: got null");
-        return toValid.matches("^[^\\s,][^,]*[^\\s,](,[^\\s,][^,]*[^\\s,])*$");
+        return Stream.of(possibleFormats).anyMatch(pattern -> toValid.matches(pattern));
     }
 }

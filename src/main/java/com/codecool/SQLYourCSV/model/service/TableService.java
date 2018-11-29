f0bbba1ae64b33e929a0bf9be7f8b5cab7fd2d9b
szymslow12 <szym.slow12@gmail.com>
2018-11-29 12:13:01 +0100

    implement createTableFromFile(), add createRow

--- src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
+++ src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 @Service
@@ -37,22 +38,52 @@ public class TableService {
             data.loadFromFile(filename);
         }
         List<String[]> dataFromCSV = data.getCSVFileData(filename);
-        Table table = new Table(createHeader(dataFromCSV.get(0)), new RowService(), filename.replace(".csv", ""));
+        String[] columnsNames = dataFromCSV.get(0);
 
-        return null;
+        Table table = new Table(
+            createHeader(columnsNames),
+            new RowService(),
+            filename.substring(0, filename.indexOf("."))
+        );
+
+        IntStream.range(1, dataFromCSV.size()).forEach(
+            i -> table.setRows(
+                table.getService().addRow(
+                    createRow(dataFromCSV.get(i), columnsNames),
+                    table.getRows()
+                )
+            )
+        );
+
+        return table;
     }
 
 
     private Row createHeader(String[] headerNames) {
         String primaryKey = headerNames[0];
         Row header = new Row(new ColumnService(), new Column<String>(primaryKey, primaryKey));
-        Stream.of(headerNames.).forEach(name -> addColumnToHeader(name, header));
+        IntStream.range(1, headerNames.length).forEach(i -> addColumnToHeader(headerNames[i], header));
         return header;
     }
 
 
     private void addColumnToHeader(String name, Row header) {
         header.setColumns(header.getService().addColumn(new Column<>(name, name), header.getColumns()));
+    }
+
+
+    private Row createRow(String[] rowValues, String[] columnsName) {
+        Row row = new Row(new ColumnService(), new Column<>(rowValues[0], columnsName[0]));
+
+        IntStream.range(1, columnsName.length).forEach(
+            i -> row.setColumns(
+                row.getService().addColumn(
+                    new Column<>(rowValues[i], columnsName[i]), row.getColumns()
+                )
+            )
+        );
+
+        return row;
     }
 
 

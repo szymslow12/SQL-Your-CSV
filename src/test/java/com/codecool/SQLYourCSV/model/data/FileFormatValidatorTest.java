5635e3ad820c336b79d00159b1a16989c1173ff8
szymslow12 <szym.slow12@gmail.com>
2018-11-29 05:25:59 +0100

    add shouldValidateReturnFalseOnIncorrectCSV_MultipleDelimiterTypesCase

--- src/test/java/com/codecool/SQLYourCSV/model/data/FileFormatValidatorTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/data/FileFormatValidatorTest.java
@@ -8,33 +8,38 @@ class FileFormatValidatorTest {
 
     @Test
     void shouldValidateReturnTrueOnCorrectCSV_CommaCase() {
-        assertTrue(FileFormatValidator.validate("text,separated,by,comma"));
+        assertTrue(FileFormatValidator.validate("text,separated,by,commas"));
     }
 
 
     @Test
     void shouldValidateReturnTrueOnCorrectCSV_SemicolonCase() {
-        assertTrue(FileFormatValidator.validate("text;separated;by;comma"));
+        assertTrue(FileFormatValidator.validate("text;separated;by;semicolons"));
     }
 
 
     @Test
     void shouldValidateReturnTrueOnCorrectCSV_ColonCase() {
-        assertTrue(FileFormatValidator.validate("text:separated:by:comma"));
+        assertTrue(FileFormatValidator.validate("text:separated:by:colons"));
     }
 
 
     @Test
     void shouldValidateReturnTrueOnCorrectCSV_TabCase() {
-        assertTrue(FileFormatValidator.validate("text   separated   by  comma"));
+        assertTrue(FileFormatValidator.validate("text\tseparated\tby\ttabs"));
     }
 
 
     @Test
-    void shouldValidateReturnFalseOnInCorrectCSV() {
+    void shouldValidateReturnFalseOnIncorrectCSV_NoDelimiterCase() {
         assertFalse(FileFormatValidator.validate("this should not pass"));
     }
 
+
+    @Test
+    void shouldValidateReturnFalseOnIncorrectCSV_MultipleDelimiterTypesCase() {
+        assertFalse(FileFormatValidator.validate("this,should;not:pass"));
+    }
 
     @Test
     void shouldValidateThrowExceptionWhenNullPassed() {

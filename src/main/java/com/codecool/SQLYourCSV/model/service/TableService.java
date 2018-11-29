e51edf7435c4c5ffd6203b2dabc3be275d0d30d7
szymslow12 <szym.slow12@gmail.com>
2018-11-29 11:37:54 +0100

    add createHeader

--- src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
+++ src/main/java/com/codecool/SQLYourCSV/model/service/TableService.java
@@ -1,10 +1,15 @@
 package com.codecool.SQLYourCSV.model.service;
 
 import com.codecool.SQLYourCSV.model.data.CSVData;
+import com.codecool.SQLYourCSV.model.datapresentation.Column;
+import com.codecool.SQLYourCSV.model.datapresentation.Row;
 import com.codecool.SQLYourCSV.model.datapresentation.Table;
 import com.codecool.SQLYourCSV.model.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class TableService {
@@ -28,7 +33,20 @@ public class TableService {
 
 
     public Table createTableFromFile(String filename) {
+        if (data.getCSVFileData(filename) == null) {
+            data.loadFromFile(filename);
+        }
+        List<String[]> dataFromCSV = data.getCSVFileData(filename);
+        Table table = new Table(createHeader(dataFromCSV.get(0)), new RowService(), filename.replace(".csv", ""));
+
         return null;
+    }
+
+
+    private Row createHeader(String[] headerNames) {
+        Row header = new Row(new ColumnService(), new Column<String>());
+        Stream.of(headerNames).forEach(name -> header.getService().addColumn(new Column<String>(), header.getColumns()));
+        return header;
     }
 
 

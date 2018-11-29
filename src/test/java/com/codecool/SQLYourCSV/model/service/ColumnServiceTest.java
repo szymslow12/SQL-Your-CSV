cb3a5b188e931fc38f18eb5263dfe1f444acf084
szymslow12 <szym.slow12@gmail.com>
2018-11-29 13:49:48 +0100

    add shouldAddColumnThrowExceptionWhenAttemptToAddNull

--- src/test/java/com/codecool/SQLYourCSV/model/service/ColumnServiceTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/service/ColumnServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,5 +33,11 @@ class ColumnServiceTest {
         Column<?> actual = service.addColumn(expected, new ArrayList<>()).get(0);
 
         assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldAddColumnThrowExceptionWhenAttemptToAddNull() {
+        assertThrows(IllegalArgumentException.class, () -> service.addColumn(null, new ArrayList<>()));
     }
 }

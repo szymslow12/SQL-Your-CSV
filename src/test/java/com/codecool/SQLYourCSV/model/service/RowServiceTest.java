39b7dd2127ad724e3391c3fca6c982b87e0b28c4
szymslow12 <szym.slow12@gmail.com>
2018-12-01 21:06:42 +0100

    add private RowService service and initializePrivateFields

--- src/test/java/com/codecool/SQLYourCSV/model/service/RowServiceTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/service/RowServiceTest.java
@@ -1,7 +1,16 @@
 package com.codecool.SQLYourCSV.model.service;
 
+import org.junit.jupiter.api.BeforeAll;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RowServiceTest {
+
+    private RowService service;
+
+    @BeforeAll
+    private void initializePrivateFields() {
+        service = new RowService();
+    }
 
 }

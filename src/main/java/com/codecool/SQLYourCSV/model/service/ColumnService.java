4c7465449e86eab96db7a808b1b5114d21c5efac
szymslow12 <szym.slow12@gmail.com>
2018-12-01 17:38:42 +0100

    add validateColumnList and use it in all public methods

--- src/main/java/com/codecool/SQLYourCSV/model/service/ColumnService.java
+++ src/main/java/com/codecool/SQLYourCSV/model/service/ColumnService.java
@@ -8,34 +8,36 @@ import java.util.stream.Stream;
 public class ColumnService {
 
     public List<Column<?>> addColumn(Column<?> toAdd, List<Column<?>> container) {
-        container.add(validateColumn(toAdd));
+        validateColumnList(container).add(validateColumn(toAdd));
         return container;
     }
 
 
     public List<Column<?>> addColumns(Column<?>[] toAdd, List<Column<?>> container) {
-        Stream.of(toAdd).forEach(column -> container.add(validateColumn(column)));
+        Stream.of(toAdd).forEach(
+            column -> validateColumnList(container).add(validateColumn(column))
+        );
         return container;
     }
 
 
     public Object getValueByName(String name, List<Column<?>> columns) {
-        return columns.stream().filter(column -> column.getName().equalsIgnoreCase(name)).findFirst().get().getValue();
+        return validateColumnList(columns).stream().filter(column -> column.getName().equalsIgnoreCase(name)).findFirst().get().getValue();
     }
 
 
     public Object getValueByIndex(int index, List<Column<?>> columns) {
-        return columns.get(index).getValue();
+        return validateColumnList(columns).get(index).getValue();
     }
 
 
     public Column<?> getColumnByName(String name, List<Column<?>> columns) {
-        return columns.stream().filter(column -> column.getName().equalsIgnoreCase(name)).findFirst().get();
+        return validateColumnList(columns).stream().filter(column -> column.getName().equalsIgnoreCase(name)).findFirst().get();
     }
 
 
     public Column<?> getColumnByIndex(int index, List<Column<?>> columns) {
-        return columns.get(index);
+        return validateColumnList(columns).get(index);
     }
 
 
@@ -44,5 +46,13 @@ public class ColumnService {
             return toValid;
         }
         throw new IllegalArgumentException("Expect Column: got null");
+    }
+
+
+    private List<Column<?>> validateColumnList(List<Column<?>> toValid) {
+        if (toValid != null) {
+            return toValid;
+        }
+        throw new IllegalArgumentException("Expect List<Column>: got null");
     }
 }

b2d12e805e60f6536bc63fd13b42354786f49fd9
szymslow12 <szym.slow12@gmail.com>
2018-12-01 21:21:37 +0100

    add shouldAddRow

--- src/test/java/com/codecool/SQLYourCSV/model/service/RowServiceTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/service/RowServiceTest.java
@@ -1,6 +1,13 @@
 package com.codecool.SQLYourCSV.model.service;
 
+import com.codecool.SQLYourCSV.model.datapresentation.Column;
+import com.codecool.SQLYourCSV.model.datapresentation.Row;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +15,25 @@ class RowServiceTest {
 
     private RowService service;
 
+    @Mock
+    private ColumnService columnService;
+
+    private Column<String> primaryKey;
+
     @BeforeAll
     private void initializePrivateFields() {
+        MockitoAnnotations.initMocks(this);
         service = new RowService();
     }
 
+
+    @Test
+    void shouldAddRow() {
+        Row toAdd = new Row(columnService);
+
+        int expected = 1;
+        int actual = service.addRow(toAdd, new ArrayList<>()).size();
+
+        assertEquals(expected, actual);
+    }
 }

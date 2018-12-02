fe7a51fd463b68e4a95ae62af6de497e2e2c323f
szymslow12 <szym.slow12@gmail.com>
2018-12-02 22:35:13 +0100

    add fields Command goesBefore/goesAfter, isOptional, add constructor

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/Rule.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/Rule.java
@@ -1,4 +1,14 @@
 package com.codecool.SQLYourCSV.model.enumeration;
 
 public enum Rule {
+
+    private Command goesBefore;
+    private Command goesAfter;
+    private boolean isOptional;
+
+    Rule(Command goesBefore, Command goesAfter, boolean isOptional) {
+        this.goesBefore = goesBefore;
+        this.goesAfter = goesAfter;
+        this.isOptional = isOptional;
+    }
 }

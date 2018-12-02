faa74b62a8d32335afd7ea563751e8df6b3b7312
szymslow12 <szym.slow12@gmail.com>
2018-12-02 22:26:22 +0100

    add field T value, setter/getter

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/Selector.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/Selector.java
@@ -2,4 +2,13 @@ package com.codecool.SQLYourCSV.model.enumeration.helpers;
 
 public class Selector<T> {
 
+    private T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }

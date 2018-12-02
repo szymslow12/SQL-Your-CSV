28a8994c83705048e3ff4f659b336392b89dbf60
szymslow12 <szym.slow12@gmail.com>
2018-12-02 22:47:42 +0100

    add import helpers.OperatorValues

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/Command.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/Command.java
@@ -1,11 +1,12 @@
 package com.codecool.SQLYourCSV.model.enumeration;
 
+import com.codecool.SQLYourCSV.model.enumeration.helpers.OperatorValues;
 import com.codecool.SQLYourCSV.model.enumeration.helpers.Selector;
 
 public enum Command {
-    SELECT(),
-    FROM(),
-    WHERE();
+    SELECT("select", false, new Selector<String[]>(), new Rule[] {Rule.SELECT_RULE_1}),
+    FROM("from", false, new Selector<String>(), new Rule[] {Rule.FROM_RULE_1, Rule.FROM_RULE_2}),
+    WHERE("where", true, new Selector<OperatorValues>(), new Rule[] {Rule.WHERE_RULE_1});
 
     private String name;
     private boolean isComparator;

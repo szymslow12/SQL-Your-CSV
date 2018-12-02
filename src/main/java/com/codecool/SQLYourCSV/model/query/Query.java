6b8072a444d2a92eba6326da1d787ac35a2db04c
szymslow12 <szym.slow12@gmail.com>
2018-12-02 21:57:17 +0100

    add Command[] comands, setters/getters, contains

--- src/main/java/com/codecool/SQLYourCSV/model/query/Query.java
+++ src/main/java/com/codecool/SQLYourCSV/model/query/Query.java
@@ -1,4 +1,20 @@
 package com.codecool.SQLYourCSV.model.query;
 
 public class Query {
+
+    private Command[] commands;
+
+    public void setCommands(Command[] commands) {
+        this.commands = commands;
+    }
+
+
+    public Command[] getCommands() {
+        return commands;
+    }
+
+
+    public boolean contains(Command toFind) {
+        return false;
+    }
 }

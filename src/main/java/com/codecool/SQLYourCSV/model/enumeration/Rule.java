5897f17acf89becd8b98c529105919420dfb8732
szymslow12 <szym.slow12@gmail.com>
2018-12-02 22:42:30 +0100

    add Rule constants

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/Rule.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/Rule.java
@@ -1,7 +1,10 @@
 package com.codecool.SQLYourCSV.model.enumeration;
 
 public enum Rule {
-    ;
+    SELECT_RULE_1(Command.FROM, null, false),
+    FROM_RULE_1(null, Command.SELECT, false),
+    FROM_RULE_2(Command.WHERE, Command.SELECT, true),
+    WHERE_RULE_1(null, Command.FROM, false);
 
     private Command goesBefore;
     private Command goesAfter;

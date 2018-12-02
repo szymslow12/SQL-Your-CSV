0bbf518d0d2b0c77d94e873e9e42ebe8091e174a
szymslow12 <szym.slow12@gmail.com>
2018-12-02 23:42:13 +0100

    add enum constants

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/Operator.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/Operator.java
@@ -5,7 +5,47 @@ import com.codecool.SQLYourCSV.model.datastructure.Column;
 import java.util.EnumSet;
 
 public enum Operator {
-    ;
+    BIGGER_THAN(">", EnumSet.of(Command.WHERE)) {
+        public boolean compare(Column<?> base, Column<?> toCompare) {
+            return false;
+        }
+    },
+
+    BIGGER_THAN_OR_EQULAS(">=", EnumSet.of(Command.WHERE)) {
+        public boolean compare(Column<?> base, Column<?> toCompare) {
+            return false;
+        }
+    },
+
+    SMALLER_THAN("<", EnumSet.of(Command.WHERE)) {
+        public boolean compare(Column<?> base, Column<?> toCompare) {
+            return false;
+        }
+    },
+
+    EQUALS("=", EnumSet.of(Command.WHERE)) {
+        public boolean compare(Column<?> base, Column<?> toCompare) {
+            return false;
+        }
+    },
+
+    LIKE("like", EnumSet.of(Command.WHERE)) {
+        public boolean compare(Column<?> base, Column<?> toCompare) {
+            return false;
+        }
+    },
+
+    AND("and", EnumSet.of(Command.WHERE)) {
+        public boolean compare(Column<?> base, Column<?> toCompare) {
+            return false;
+        }
+    },
+
+    OR("or", EnumSet.of(Command.WHERE)) {
+        public boolean compare(Column<?> base, Column<?> toCompare) {
+            return false;
+        }
+    };
 
     private String value;
     private EnumSet<Command> belongsTo;

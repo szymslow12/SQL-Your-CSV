95ffeb54feac0a2568a181140ad2573462b085ba
szymslow12 <szym.slow12@gmail.com>
2018-12-02 22:53:30 +0100

    add field String value, EnumSet<Command> belongsTo, constructor

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/Operator.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/Operator.java
@@ -1,4 +1,15 @@
 package com.codecool.SQLYourCSV.model.enumeration;
 
+import java.util.EnumSet;
+
 public enum Operator {
+    ;
+
+    private String value;
+    private EnumSet<Command> belongsTo;
+
+    Operator(String value, EnumSet<Command> belongsTo) {
+        this.value = value;
+        this.belongsTo = belongsTo;
+    }
 }

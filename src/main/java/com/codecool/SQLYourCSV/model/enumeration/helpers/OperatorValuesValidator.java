2e8e62a28fc54cd7e1110c5f90928db19ccf26eb
szymslow12 <szym.slow12@gmail.com>
2018-12-03 16:10:02 +0100

    add areBooleans, clean code

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidator.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidator.java
@@ -5,20 +5,30 @@ import com.codecool.SQLYourCSV.model.datastructure.Column;
 public class OperatorValuesValidator {
 
     public static boolean areSameType(Column<?> base, Column<?> toCheck) {
-        if (areColumnsNull(base, toCheck)) throw new IllegalArgumentException("Expect Column<?>: got null");
+        if (areColumnsNull(base, toCheck))
+            throw new IllegalArgumentException("Expect Column<?>: got null");
         return base.getValue().getClass().equals(toCheck.getValue().getClass());
     }
 
 
     public static boolean areNumbers(Column<?> toCheck, Column<?> toCheck1) {
-        if (areColumnsNull(toCheck, toCheck1)) throw new IllegalArgumentException("Expect Column<?>: got null");
+        if (areColumnsNull(toCheck, toCheck1))
+            throw new IllegalArgumentException("Expect Column<?>: got null");
         return toCheck.getValue() instanceof Number && toCheck1.getValue() instanceof Number;
     }
 
 
     public static boolean areStrings(Column<?> toCheck, Column<?> toCheck1) {
-        if (areColumnsNull(toCheck, toCheck1)) throw new IllegalArgumentException("Expect Column<?>: got null");
+        if (areColumnsNull(toCheck, toCheck1))
+            throw new IllegalArgumentException("Expect Column<?>: got null");
         return toCheck.getValue() instanceof String && toCheck1.getValue() instanceof String;
+    }
+
+
+    public static boolean areBooleans(Column<?> toCheck, Column<?> toCheck1) {
+        if (areColumnsNull(toCheck, toCheck1))
+            throw new IllegalArgumentException("Expect Column<?>: got null");
+        return toCheck.getValue() instanceof Boolean && toCheck1.getValue() instanceof Boolean;
     }
 
 

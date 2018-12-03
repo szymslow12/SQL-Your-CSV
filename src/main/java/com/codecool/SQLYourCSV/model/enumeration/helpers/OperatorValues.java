71e62c47bb2cb4fe81003ea55983d83ae632e649
szymslow12 <szym.slow12@gmail.com>
2018-12-03 18:53:37 +0100

    add fields Operator operator, String[] toCompare, setters/getters

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValues.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValues.java
@@ -1,4 +1,28 @@
 package com.codecool.SQLYourCSV.model.enumeration.helpers;
 
+import com.codecool.SQLYourCSV.model.enumeration.Operator;
+
 public class OperatorValues {
+
+    private Operator operator;
+    private String[] toCompare;
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+
+    public void setToCompare(String[] toCompare) {
+        this.toCompare = toCompare;
+    }
+
+
+    public String[] getToCompare() {
+        return toCompare;
+    }
 }

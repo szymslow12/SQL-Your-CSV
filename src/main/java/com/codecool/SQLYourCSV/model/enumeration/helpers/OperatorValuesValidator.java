f8570401a76cb4aad87e86cd32262dd1cb5f154a
szymslow12 <szym.slow12@gmail.com>
2018-12-03 12:51:56 +0100

    add areNumbers/areStrings

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidator.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidator.java
@@ -7,4 +7,14 @@ public class OperatorValuesValidator {
     public static boolean areSameType(Column<?> base, Column<?> toCheck) {
         return base.getValue().getClass().equals(toCheck.getValue().getClass());
     }
+
+
+    public static boolean areNumbers(Column<?> toCheck, Column<?> toCheck1) {
+        return toCheck.getValue() instanceof Number && toCheck1.getValue() instanceof Number;
+    }
+
+
+    public static boolean areStrings(Column<?> toCheck, Column<?> toCheck1) {
+        return toCheck.getValue() instanceof String && toCheck1.getValue() instanceof String;
+    }
 }

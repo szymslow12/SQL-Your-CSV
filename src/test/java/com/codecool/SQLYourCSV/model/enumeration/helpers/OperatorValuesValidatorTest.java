47fb8479f84bf6ecebf8884015a57ba47c0a8391
szymslow12 <szym.slow12@gmail.com>
2018-12-03 19:11:48 +0100

    add test cases for OperatorValuesValidator.areNumbers()

--- src/test/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidatorTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidatorTest.java
@@ -29,4 +29,37 @@ class OperatorValuesValidatorTest {
         assertThrows(IllegalArgumentException.class,
             () -> OperatorValuesValidator.areSameType(null, null));
     }
+
+
+    @Test
+    void testAreNumbers_NumbersCase() {
+        Column<Integer> toTest = new Column<>(1, "name");
+
+        assertTrue(OperatorValuesValidator.areNumbers());
+    }
+
+
+    @Test
+    void testAreNumbers_OneNumberOneDifferent() {
+        Column<Integer> integerColumn = new Column<>(1, "name");
+        Column<String> stringColumn = new Column<>("value", "name");
+
+        assertFalse(OperatorValuesValidator.areNumbers(integerColumn, stringColumn));
+    }
+
+
+    @Test
+    void testAreNumbers_NotNumbers() {
+        Column<String> toTest1 = new Column<>("value", "name");
+        Column<String> toTest2 = new Column<>("value", "name");
+
+        assertFalse(OperatorValuesValidator.areNumbers(toTest1, toTest2));
+    }
+
+
+    @Test
+    void testAreNumbersThrowsExceptionWhenNullsIsPassed() {
+        assertThrows(IllegalArgumentException.class,
+            () -> OperatorValuesValidator.areNumbers(null, null));
+    }
 }

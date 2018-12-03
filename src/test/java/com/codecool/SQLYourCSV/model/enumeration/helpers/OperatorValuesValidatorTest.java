ab2489bd90a7de70b41ea3c266793042723b97f2
szymslow12 <szym.slow12@gmail.com>
2018-12-03 19:05:11 +0100

    add test cases for OperatorValuesValidator.areSameType()

--- src/test/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidatorTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValuesValidatorTest.java
@@ -1,7 +1,32 @@
 package com.codecool.SQLYourCSV.model.enumeration.helpers;
 
+import com.codecool.SQLYourCSV.model.datastructure.Column;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OperatorValuesValidatorTest {
 
+    @Test
+    void testAreSameType_SameTypes() {
+        Column<String> toTest = new Column<>("value", "name");
+
+        assertTrue(OperatorValuesValidator.areSameType(toTest, toTest));
+    }
+
+
+    @Test
+    void testAreSameType_NotSameTypes() {
+        Column<String> stringColumn = new Column<>("value", "name");
+        Column<Integer> integerColumn = new Column<>(1, "name");
+
+        assertFalse(OperatorValuesValidator.areSameType(stringColumn, integerColumn));
+    }
+
+
+    @Test
+    void testAreSameTypeThrowsExceptionWhenNullsArePassed() {
+        assertThrows(IllegalArgumentException.class,
+            () -> OperatorValuesValidator.areSameType(null, null));
+    }
 }

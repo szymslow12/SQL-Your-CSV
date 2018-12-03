5f1408573c44ee0ad1c586f97baac5ff332f27bb
szymslow12 <szym.slow12@gmail.com>
2018-12-03 01:52:24 +0100

    add OperatorValidator with validate()

---
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/helpers/OperatorValidator.java
@@ -0,0 +1,10 @@
+package com.codecool.SQLYourCSV.model.enumeration.helpers;
+
+import com.codecool.SQLYourCSV.model.datastructure.Column;
+
+public class OperatorValidator {
+
+    public boolean validate(Column<?> base, Column<?> toCheck) {
+        return false;
+    }
+}

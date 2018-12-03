55dd2f848da6c5ccfdeec27bb2961c078c444e41
szymslow12 <szym.slow12@gmail.com>
2018-12-03 02:12:14 +0100

    add shouldEQUALS_CompareProperlySameColumn_TwoReferences/compare

--- src/test/java/com/codecool/SQLYourCSV/model/enumeration/OperatorTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/enumeration/OperatorTest.java
@@ -7,11 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OperatorTest {
 
+
+    private boolean compare(Column<?> first, Column<?> second) {
+        return Operator.EQUALS.compare(first, second);
+    }
+
+
     @Test
     void shouldEQUALS_CompareProperlySameColumn_OneReference() {
         Column<String> toCompare = new Column<>("value", "name");
 
-        assertTrue(Operator.EQUALS.compare(toCompare, toCompare));
+        assertTrue(compare(toCompare, toCompare));
+    }
+
+
+    @Test
+    void shouldEQUALS_CompareProperlySameColumn_TwoReferences() {
+        Column<String> first = new Column<>("value", "name");
+        Column<String> second = new Column<>("value", "name");
+
+        assertTrue(compare(first, second));
     }
 
 }

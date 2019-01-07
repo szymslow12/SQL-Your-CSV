168df0920ee68eb2713b829ff7d9e0c298a30c50
szymslow12 <szym.slow12@gmail.com>
2019-01-07 17:47:22 +0100

    extract getSingleData, getAllData to Data interface, CSVData impl Data

--- src/main/java/com/codecool/SQLYourCSV/model/data/Data.java
+++ src/main/java/com/codecool/SQLYourCSV/model/data/Data.java
@@ -1,4 +1,11 @@
 package com.codecool.SQLYourCSV.model.data;
 
+import java.util.List;
+import java.util.Map;
+
 public interface Data {
+
+    List<String[]> getSingleData(String dataName);
+
+    Map<String, List<String[]>> getAllData();
 }

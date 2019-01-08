4a92e5e1236554f3093153bbc6f2b2e3737acf6b
szymslow12 <szym.slow12@gmail.com>
2019-01-08 15:53:06 +0100

    add isCommand, findIndex

--- src/main/java/com/codecool/SQLYourCSV/model/query/QueryParser.java
+++ src/main/java/com/codecool/SQLYourCSV/model/query/QueryParser.java
@@ -4,9 +4,11 @@ import com.codecool.SQLYourCSV.model.enumeration.Command;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.function.Function;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class QueryParser {
@@ -30,8 +32,27 @@ public class QueryParser {
 
         System.out.println(Arrays.toString(queryParts));
         Stream.of(queryParts).forEach(p -> System.out.println(p));
-        Stream.of(commands).forEach(e -> System.out.println(e));
+        Stream.of(commands).forEach(e -> System.out.println(e.selector().getValue()));
+        Stream.of(commands).forEach(command -> {
+            List<String> queryPartsAsList = Arrays.asList(queryParts);
+            IntStream.range(findIndex(command, queryPartsAsList), queryPartsAsList.size()).map(i -> );
+        });
         query.setCommands(commands);
         return null;
+    }
+
+
+    private static boolean isCommand(String toCheck) {
+        return Stream.of(Command.values()).anyMatch(command -> command.getName().equalsIgnoreCase(toCheck));
+    }
+
+
+    private static int findIndex(Command command, List<String> queryParts) {
+        IntPredicate findIndex = i -> command.getName().equalsIgnoreCase(queryParts.get(i));
+        OptionalInt index = IntStream.range(0, queryParts.size()).filter(findIndex).findFirst();
+        if (index.isPresent()) {
+            return index.getAsInt();
+        }
+        throw new IllegalArgumentException(String.format("Query is not valid missing %s", command.getName()));
     }
 }

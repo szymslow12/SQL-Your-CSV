72ee22ce901bb922cf0e65807048ee7090bdf765
Riczard <riczardkiller@gmail.com>
2019-01-08 22:31:19 +0100

    add main method

---
+++ src/main/java/com/codecool/SQLYourCSV/SheetQL.java
@@ -0,0 +1,12 @@
+package com.codecool.SQLYourCSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetQL {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetQL.class, args);
+    }
+}

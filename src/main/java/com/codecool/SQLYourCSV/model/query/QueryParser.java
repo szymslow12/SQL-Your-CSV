34463b7f0e32e3d3c2b4b7d4a7cbe95e3b2e8fb1
szymslow12 <szym.slow12@gmail.com>
2019-01-08 10:32:23 +0100

    add if stmt to check toParse if not null and has semicolon

--- src/main/java/com/codecool/SQLYourCSV/model/query/QueryParser.java
+++ src/main/java/com/codecool/SQLYourCSV/model/query/QueryParser.java
@@ -1,8 +1,30 @@
 package com.codecool.SQLYourCSV.model.query;
 
+import com.codecool.SQLYourCSV.model.enumeration.Command;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class QueryParser {
 
     public static Query parse(String toParse) {
+        if (toParse == null) {
+            throw new IllegalArgumentException("Query is null!");
+        }
+        if (toParse.charAt(toParse.length() - 1) != ";".charAt(0)) {
+            throw new IllegalArgumentException("Missing semicolon!");
+        }
+        Query query = new Query();
+        String[] queryParts = toParse.split(" |, |,");
+
+        System.out.println(Arrays.toString(queryParts));
+        Stream.of(queryParts).forEach(p -> System.out.println(p));
+        commandList.forEach(e -> System.out.println(e));
         return null;
     }
 }

40d125ec5d278b5e6416b9f4d9b86bbb74b2e587
szymslow12 <szym.slow12@gmail.com>
2019-01-08 16:35:27 +0100

    adjust changes to changes in Selector

--- src/main/java/com/codecool/SQLYourCSV/model/enumeration/Command.java
+++ src/main/java/com/codecool/SQLYourCSV/model/enumeration/Command.java
@@ -6,16 +6,16 @@ import com.codecool.SQLYourCSV.model.enumeration.helpers.Selector;
 import java.util.EnumSet;
 
 public enum Command {
-    SELECT("select", false, new Selector<String[]>(), EnumSet.of(Rule.SELECT_RULE_1)),
-    FROM("from", false, new Selector<String>(), EnumSet.of(Rule.FROM_RULE_1, Rule.FROM_RULE_2)),
-    WHERE("where", true, new Selector<OperatorValues>(), EnumSet.of(Rule.WHERE_RULE_1));
+    SELECT("select", false, new Selector(), EnumSet.of(Rule.SELECT_RULE_1)),
+    FROM("from", false, new Selector(), EnumSet.of(Rule.FROM_RULE_1, Rule.FROM_RULE_2)),
+    WHERE("where", true, new Selector(), EnumSet.of(Rule.WHERE_RULE_1));
 
     private String name;
     private boolean isComparator;
-    private Selector<?> selector;
+    private Selector selector;
     private EnumSet<Rule> rules;
 
-    Command(String name, boolean isComparator, Selector<?> selector, EnumSet<Rule> rules) {
+    Command(String name, boolean isComparator, Selector selector, EnumSet<Rule> rules) {
         this.name = name;
         this.isComparator = isComparator;
         this.selector = selector;
@@ -33,7 +33,7 @@ public enum Command {
     }
 
 
-    public Selector<?> selector() {
+    public Selector selector() {
         return selector;
     }
 

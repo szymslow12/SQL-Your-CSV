5014f30d40152ba05959b93e90ab4c9ce567859f
szymslow12 <szym.slow12@gmail.com>
2019-01-09 17:02:20 +0100

    add send(),change sendQuery -> if stmt which check if query is not empty

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,7 +4,28 @@ function adjustHeight(elem) {
 
 
 function sendQuery() {
-    let textArea = document.getElementById("query");
-    let query = textArea.value;
-    alert(query);
+    let query = document.getElementById("query").value;
+
+    if (query) {
+        send(query);
+    } else {
+        alert("Query can't be empty!");
+    }
+}
+
+
+function send(query) {
+    let httpExchange = new XMLHttpRequest();
+
+    httpExchange.open("POST", "/sheetql", true);
+    httpExchange.setRequestHeader("Content-Type", "text/plain");
+
+    httpExchange.onreadystatechange = () => {
+        if (httpExchange.status === 200 && httpExchange.readyState === 4) {
+            document.write(httpExchange.response);
+            document.close();
+        }
+    };
+
+    httpExchange.send(query);
 }

59f0a267d04929bb68be783c0844b93ac3a9c708
szymslow12 <szym.slow12@gmail.com>
2019-01-09 16:49:38 +0100

    add sendQuery

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,10 @@
 function adjustHeight(elem) {
     elem.style.height = (window.innerHeight - document.getElementById("footer").offsetHeight) + "px";
 }
+
+
+function sendQuery() {
+    let textArea = document.getElementById("query");
+    let query = textArea.value;
+    alert(query);
+}

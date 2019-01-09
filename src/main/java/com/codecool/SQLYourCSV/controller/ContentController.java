8605d6d86508135befc9e8b8c7828f728b917425
szymslow12 <szym.slow12@gmail.com>
2019-01-09 17:04:30 +0100

    add field TableService,add mapping to methods,add param to result()

--- src/main/java/com/codecool/SQLYourCSV/controller/ContentController.java
+++ src/main/java/com/codecool/SQLYourCSV/controller/ContentController.java
@@ -1,25 +1,30 @@
 package com.codecool.SQLYourCSV.controller;
 
+import com.codecool.SQLYourCSV.model.service.TableService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ContentController {
 
-    @GetMapping
-    public String contentForm() {
+    @Autowired
+    private TableService service;
+
+    @GetMapping("/sheetql")
+    public String content() {
         return "sheetQl";
     }
 
-    @PostMapping
-    public String resultForm(HttpServletRequest request, Model model) {
-        String name = request.getParameter("name");
+    @PostMapping("/sheetql")
+    public String result(Model model, @RequestBody String query) {
+//        String name = request.getParameter("name");
 
 //        model.addAttribute("name". name);
+        System.out.println(query);
         return "sheetQl";
     }
 }

615f5aa74b497a868c514674c03164462d8d3462
piotrkaminski95 <pkaminki95@gmail.com>
2019-01-09 12:54:54 +0100

    add GoogleAuthorizeUtil and SheetsServiceUtil

---
+++ src/main/java/com/codecool/SQLYourCSV/model/service/SheetsServiceUtil.java
@@ -0,0 +1,27 @@
+package com.codecool.SQLYourCSV.model.service;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+@Service
+public class SheetsServiceUtil {
+    @Autowired
+    private static Credential credential;
+    private static final String APPLICATION_NAME = "Google Sheets Demo";
+
+    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        Credential credential = GoogleAuthorizeUtil.authorize();
+        return new Sheets.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(), credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+}

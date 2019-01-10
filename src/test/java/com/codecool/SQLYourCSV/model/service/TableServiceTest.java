c30db28f8e63815c73adf98c07c65ed8796a6322
szymslow12 <szym.slow12@gmail.com>
2019-01-10 10:51:19 +0100

    add fields @Mock  Data/Query, TableService

--- src/test/java/com/codecool/SQLYourCSV/model/service/TableServiceTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/service/TableServiceTest.java
@@ -1,7 +1,20 @@
 package com.codecool.SQLYourCSV.model.service;
 
+import com.codecool.SQLYourCSV.model.data.Data;
+import com.codecool.SQLYourCSV.model.query2.Query;
+import org.mockito.Mock;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TableServiceTest {
+
+    @Mock
+    private Data data;
+
+    @Mock
+    private Query query;
+
+    private TableService tableService;
+
 
 }

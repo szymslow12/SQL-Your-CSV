a56354819efdb7c745d2860453b548378eb0dbdf
Riczard <riczardkiller@gmail.com>
2019-01-10 01:15:35 +0100

    implement add columns

--- src/main/java/com/codecool/SQLYourCSV/model/query2/QueryParser.java
+++ src/main/java/com/codecool/SQLYourCSV/model/query2/QueryParser.java
@@ -2,16 +2,18 @@ package com.codecool.SQLYourCSV.model.query2;
 
 import com.codecool.SQLYourCSV.model.enumeration.Statements;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class QueryParser {
 
-    private List<String> splitedQuery;
+    private List<String> splitedQuery = new ArrayList<>();
     private Query queryObject = new Query();
 
     public QueryParser(String query) {
-        this.splitedQuery = getSplitedQuery(query);
+        createQueryList(query);
     }
 
     public void queryParser() {
@@ -23,20 +25,25 @@ public class QueryParser {
 
         deleteStatementFromQuery(statement);
 
-        getColumnsName();
+        String[] columnsName = getColumnsName();
+
+        queryObject.setColumns(columnsName);
 
         System.out.println(queryObject.toString());
 
     }
 
-    private List<String> getSplitedQuery(String query) {
-        return Arrays.asList(query.split(" +|, +|,"));
+    private void createQueryList(String query) {
+        String[] split = query.toUpperCase().split(" +|, +|,");
+        Collections.addAll(splitedQuery, split);
     }
 
     private void checkQueryIsProper() {
         if (isEmpty()) throw new IllegalArgumentException("Query is not complete!");
         if (!isSemicolonAtEnd()) throw new IllegalArgumentException("Missing semicolon!");
         if (!isStatementInQuery()) throw new IllegalArgumentException("Any statement in query or in wrong position");
+        if (!isFromKeywordInQuery()) throw new IllegalArgumentException("Missing from keyword!");
+        if(missingColumnNames()) throw new IllegalArgumentException("Columns names in wrong position or any columns name are entered!");
     }
 
     private boolean isEmpty() {
@@ -64,6 +71,17 @@ public class QueryParser {
         return false;
     }
 
+    private boolean isFromKeywordInQuery() {
+        for (String s : splitedQuery) {
+            if (s.equalsIgnoreCase("from")) return true;
+        }
+        return false;
+    }
+
+    private boolean missingColumnNames() {
+        return splitedQuery.get(0).equalsIgnoreCase("from");
+    }
+
     private String getStatement() {
         int stmtIndexPos1 = 0;
         int stmtIndexPos2 = 1;
@@ -87,13 +105,23 @@ public class QueryParser {
 
     private void deleteStatementFromQuery(String statement) {
         String[] s = statement.split(" ");
-        for (String string : s) {
-            splitedQuery.remove(string);
+        for (String word : s) {
+            splitedQuery.remove(word);
         }
     }
 
-    private void getColumnsName() {
+    private String[] getColumnsName() {
+        int fromKeyWordIndex = getFromKeywordIndex();
+        String[] strings = splitedQuery.stream().toArray(String[]::new);
+        return Arrays.copyOf(strings, fromKeyWordIndex);
 
+    }
+
+    private int getFromKeywordIndex() {
+        for(int i = 0; i < splitedQuery.size(); i++){
+            if(splitedQuery.get(i).equalsIgnoreCase("FROM")) return i;
+        }
+        return 0;
     }
 
 }

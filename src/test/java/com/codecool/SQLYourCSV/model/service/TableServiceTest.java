b9f733d86ff21659c05f520ed89b351950e71b65
szymslow12 <szym.slow12@gmail.com>
2019-01-10 11:03:57 +0100

    add initMock, createTestTableData

--- src/test/java/com/codecool/SQLYourCSV/model/service/TableServiceTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/service/TableServiceTest.java
@@ -2,9 +2,15 @@ package com.codecool.SQLYourCSV.model.service;
 
 import com.codecool.SQLYourCSV.model.data.Data;
 import com.codecool.SQLYourCSV.model.query2.Query;
+import org.junit.jupiter.api.BeforeAll;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class TableServiceTest {
 
@@ -16,5 +22,20 @@ class TableServiceTest {
 
     private TableService tableService;
 
+
+    @BeforeAll
+    private void initMocks() {
+        MockitoAnnotations.initMocks(this);
+        when(query.getTableName()).thenReturn("tableName");
+        when(query.getColumns()).thenReturn(new String[]{"column_one", "column_two", "column_three"});
+        when(query.getClauseName()).thenReturn("WHERE");
+        when(query.getStatement()).thenReturn("SELECT");
+        when(data.getSingleData(anyString())).thenReturn();
+    }
+
+
+    private List<String[]> createTestTableData() {
+
+    }
 
 }

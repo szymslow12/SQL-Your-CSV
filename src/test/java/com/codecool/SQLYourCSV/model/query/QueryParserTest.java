6898e85d9c454cb75987b07bd07f028006858f9c
szymslow12 <szym.slow12@gmail.com>
2019-01-11 11:01:19 +0100

    fix QueryParser -> add new test case for it -> all test passed

--- src/test/java/com/codecool/SQLYourCSV/model/query/QueryParserTest.java
+++ src/test/java/com/codecool/SQLYourCSV/model/query/QueryParserTest.java
@@ -6,23 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class QueryParserTest {
 
-//    @Test
-//    void testQueryWitchoutWhereExpectedSuccess() {
-//        String statement = "Select * from table_name;";
-//        String expected = "Query{statement='SELECT', columns=[*], tableName='table_name', clauseName=''," +
-//                " clauseCondition='', clauseValue=''}";
-//        String result = QueryParser.parse(statement).toString();
-//        Assert.assertEquals(expected, result);
-//    }
+    @Test
+    void testQueryWitchoutWhereExpectedSuccess() {
+        String statement = "Select * from table_name;";
+        String expected = "Query{statement='SELECT', columns=[*], tableName='table_name', clauseName=''," +
+                " clauseCondition='', clauseValue=''}";
+        String result = QueryParser.parse(statement).toString();
+        Assert.assertEquals(expected, result);
+    }
 
-//    @Test
-//    void testQueryWithWhereExpectedSuccess() {
-//        String statement = "Select * from table_name where cond='result';";
-//        String expected = "Query{statement='SELECT', columns=[*], tableName='table_name', clauseName='where'," +
-//                " clauseCondition='cond', clauseValue='result'}";
-//        String result = QueryParser.parse(statement).toString();
-//        Assert.assertEquals(expected, result);
-//    }
+    @Test
+    void testQueryWithWhereExpectedSuccess() {
+        String statement = "Select * from table_name where cond='result';";
+        String expected = "Query{statement='SELECT', columns=[*], tableName='table_name', clauseName='where'," +
+                " clauseCondition='cond', clauseValue='result'}";
+        String result = QueryParser.parse(statement).toString();
+        Assert.assertEquals(expected, result);
+    }
 
     @Test
     void testQueryWhenNoSemicolonAtEndExpectIllegalExcepion(){
@@ -42,14 +42,14 @@ class QueryParserTest {
                 });
     }
 
-//    @Test
-//    void testQueryWithTooMuchSpacesExpectedSuccess() {
-//        String statement = "Select * from          table_name    where cond   =    'result';";
-//        String expected = "Query{statement='SELECT', columns=[*], tableName='table_name', clauseName='where'," +
-//                " clauseCondition='cond', clauseValue='result'}";
-//        String result = QueryParser.parse(statement).toString();
-//        Assert.assertEquals(expected, result);
-//    }
+    @Test
+    void testQueryWithTooMuchSpacesExpectedSuccess() {
+        String statement = "Select * from          table_name    where cond   =    'result';";
+        String expected = "Query{statement='SELECT', columns=[*], tableName='table_name', clauseName='where'," +
+                " clauseCondition='cond', clauseValue='result'}";
+        String result = QueryParser.parse(statement).toString();
+        Assert.assertEquals(expected, result);
+    }
 
     @Test
     void testQueryWithTooMuchColumnsInQueryExpectedIllegalExpcetion() {
@@ -87,4 +87,16 @@ class QueryParserTest {
                 });
     }
 
+
+    @Test
+    void testQueryWithMoreThanTwoColumnsSuccess() {
+        String statment = "Select col1, col2, col3 From table_name;";
+
+        String expected = "Query{statement='SELECT', columns=[col1, col2, col3], tableName='table_name', clauseName=''," +
+                " clauseCondition='', clauseValue=''}";
+
+        String actual = QueryParser.parse(statment).toString();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
